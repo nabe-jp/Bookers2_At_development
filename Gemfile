1bb2d048db143f36e4e21d6e50ce9bb568f9ced1
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-03-25 03:11:42 +0000

    Bookers2_app-iine_come_app_At6_25/03/25

--- Gemfile
+++ Gemfile
@@ -59,3 +59,5 @@ gem 'devise'
 gem 'kaminari','~> 1.2.1'
 
 gem 'net-smtp'
+
+gem 'jquery-rails'
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,21 +1,18 @@
 class FavoritesController < ApplicationController
 
   def create
-    
-    book = Book.find(params[:book_id])
-    if book.user.id != current_user.id    # 自分の投稿にはいいねを出来ないようにする
-      favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    if @book.user.id != current_user.id    # 自分の投稿にはいいねを出来ないようにする
+      favorite = current_user.favorites.new(book_id: @book.id)
       favorite.save
     end
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    if book.user.id != current_user.id
-      favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    if @book.user.id != current_user.id
+      favorite = current_user.favorites.find_by(book_id: @book.id)
       favorite.destroy
     end
-    redirect_to request.referer   # 回答より抜粋こちらでも前の画面に戻れる
   end
 end
