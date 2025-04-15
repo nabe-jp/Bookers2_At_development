1f936ab0998d15696731c558075677157cc1d7ac
nabe <197488176+nabe-jp@users.noreply.github.com>
2025-04-15 10:52:23 +0000

    Bookers2_At_development_25/04/15_1

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,13 +2,23 @@ class SearchesController < ApplicationController
   before_action :authenticate_user!
 
   def search
-    @range = params[:range]
+    if (params[:range] == "User") || (params[:range] == "Book")
+      @range = params[:range]
+    else
+      @range = "Tag"
+    end
     @word = params[:word]
 
     if @range == "User"
+      @type = "index" 
       @users = User.looks(params[:search], params[:word]).page(params[:page])
-    elsif @range == "User"
+    elsif @range == "Book"
       @books = Book.looks(params[:search], params[:word]).page(params[:page])
+    elsif @range == "Tag"
+      @books = Book.where("tag LIKE?","%#{@word}%").page(params[:page])
+    else
+      flash[:alert] = "エラー"
     end
+    render "searches/result"
   end
 end
